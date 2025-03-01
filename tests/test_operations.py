390b430695c1ff485694f7250656cdbac118feaf
subramanayam95 <cheenepallisubramanyam95@gmail.com>
2025-03-01 11:14:10 +0530

    second commit

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,7 @@
 from src.maths_oprations import add,sub
 def test_add():
     assert add(5, 3) == 8
-    assert add(-1, -1) == 0
+    assert add(-1, -1) == -2
 def test_sub():
     assert sub(5, 3) == 2
     assert sub(4, 3) == 1
